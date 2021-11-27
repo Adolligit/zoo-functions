becf3465ce6bae7aa9d3b4e2cc11a6d9faef4553
Adolligit <adelson.onod@gmail.com>
2021-11-27 20:32:51 -0300

    exercise one completed

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if(ids.length == 0) return []
+
+  return data.species.filter(specie => ids.find(id => specie.id === id))
 }
 
 module.exports = getSpeciesByIds;
+
+
+// let x = ["adelson", "adelson", "lima", "adelson"];
+
+// console.log(x.find(e => e === "adelson"));
