28e377ee80bb166726fdbaaebae145aafcee645d
Adolligit <adelson.onod@gmail.com>
2021-12-02 03:21:45 -0300

    requirement five, countAnimals, completed; passed on jest and lint.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  if (!animal) {
+    return data.species.reduce((object, specie) => {
+      // código feio por causa do lint
+      const lintFix = object;
+      lintFix[specie.name] = specie.residents.length;
+      return lintFix;
+    }, {});
+  }
 
+  const { specie: specieName, sex } = animal;
+
+  if (sex) {
+    return data.species
+      .find((specie) => specie.name === specieName)
+      .residents.reduce((total, resident) => ((resident.sex === 'female') ? total + 1 : total), 0);
+  }
+  return data.species
+    .find((specie) => specie.name === specieName).residents.length;
+}
 module.exports = countAnimals;
