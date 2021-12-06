973b3031079e0ad2f0d0441b18535c3c2ce0637a
Adolligit <adelson.onod@gmail.com>
2021-12-06 18:09:55 -0300

    jest, first test passed

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    return species.reduce((acc, specie) => {
+      const response = acc[`${specie.location}`];
+
+      if (response) {
+        acc[`${specie.location}`].push(specie.name);
+      } else {
+        acc[`${specie.location}`] = [specie.name];
+      }
+      return acc;
+    }, {});
+  }
 }
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
