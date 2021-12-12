71be93addceb8aac72e72569f227f19927b3c610
Adolligit <adelson.onod@gmail.com>
2021-12-12 14:26:46 -0300

    código não testado, porém salvo para futuras alterações

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -3,20 +3,27 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getAnimalMap(options) {
-  if (!options) {
-    return species.reduce((acc, specie) => {
-      const response = acc[`${specie.location}`];
+  return species.reduce((acc, specie) => {
+    // acc: {} <- location: [] <- specie.name: {} <- name.redents []
+    const response = acc[`${specie.location}`]; // verificando existência do atributo.
+    
+    // if (!response) acc[`${specie.location}`] = []; // caso não exista o atributo, crie esta posição e adicione um array
+    
+    if (!options || (options.sex === 'female' && Object.keys(options).length < 2) || options.sorted) {
+      acc[`${specie.location}`].push(specie.name);
+    } else {
+      const nameResidents = {};
+      
+      nameResidents[`${specie.name}`] = specie.residents.map((resident) => resident.name);
+      acc[`${specie.location}`].push(nameResidents);
+    }
 
-      if (response) {
-        acc[`${specie.location}`].push(specie.name);
-      } else {
-        acc[`${specie.location}`] = [specie.name];
-      }
-      return acc;
-    }, {});
-  }
+    return acc;
+  }, {});
 }
 
-console.log(getAnimalMap());
+console.log();
+console.log(getAnimalMap({ sex: 'female', sorted: true }));
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
