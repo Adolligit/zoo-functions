085a8d4f2e7945ad27c28e7489deb87ba171f5cc
Adolligit <adelson.onod@gmail.com>
2021-12-17 14:49:59 -0300

    EsLint e Jest aprovados. Requisito / Exercício: finalizado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const weekday = {
+  Tuesday: {},
+  Wednesday: {},
+  Thursday: {},
+  Friday: {},
+  Saturday: {},
+  Sunday: {},
+  Monday: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+};
+
+function creatingAttributesCalendar(species, day) {
+  const hours = data.hours[day];
+
+  return {
+    officeHour: `Open from ${hours.open}am until ${hours.close}pm`,
+    exhibition: species
+      .filter((specie) => specie.availability.includes(day))
+      .map((specie) => specie.name),
+  };
 }
+
+function getAvailabilitySpecie(arraySpecies, specieName) {
+  return arraySpecies.find((specie) => specie.name === specieName);
+}
+
+function getSchedule(scheduleTarget) {
+  const hasSpecies = getAvailabilitySpecie(data.species, scheduleTarget);
+
+  if (Object.keys(weekday).includes(scheduleTarget)) {
+    return { [scheduleTarget]: weekday[scheduleTarget] };
+  }
+
+  if (!hasSpecies) {
+    return Object.keys(weekday).reduce((acc, day) => {
+      if (day !== 'Monday') acc[day] = creatingAttributesCalendar(data.species, day);
+      return acc;
+    }, weekday);
+  }
+
+  return hasSpecies.availability;
+}
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
