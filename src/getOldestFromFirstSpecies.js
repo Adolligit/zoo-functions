be47418fb996c72ae51cd111aea8121fd5b4aef8
Adolligit <adelson.onod@gmail.com>
2021-12-18 01:53:13 -0300

    EsLint e Jest: Aprovados. Requisito / Exercício: finalizado.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const older = species
+    .find((specie) => specie.id === firstSpecie)
+    .residents.reduce((acc, resident) => ((acc.age > resident.age) ? acc : resident));
+
+  return Object.values(older);
 }
+
+console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = getOldestFromFirstSpecies;
