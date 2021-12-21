238f528da401ba0a25e4c8d000b483a5075f9fd9
Adolligit <adelson.onod@gmail.com>
2021-12-21 15:02:29 -0300

    jest enloqueceu

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function searchEmployee(value) {
+  if (value) {
+    return [employees.find(({ id, firstName, lastName }) =>
+      id === value.id
+      || firstName === value.name
+      || lastName === value.name)];
+  }
+  return employees.filter((e) => e);
+}
+
+function includeSpecies(array) {
+  array.forEach((acc) => {
+    const objSpecie = species.filter((specie) => acc.responsibleFor.includes(specie.id));
+    acc.species = objSpecie.map((e) => e.name);
+    acc.locations = objSpecie.map((e) => e.location);
+  });
+  return array;
+}
+
+function getEmployeesCoverage(param) {
+  const filteredEmployees = searchEmployee(param);
+
+  filteredEmployees.forEach((acc) => {
+    acc.fullName = `${acc.firstName} ${acc.lastName}`;
+  });
+
+  const transformedEmployees = includeSpecies(filteredEmployees);
+
+  transformedEmployees.forEach((e) => {
+    delete e.firstName;
+    delete e.lastName;
+    delete e.managers;
+    delete e.responsibleFor;
+  });
+
+  return (param) ? transformedEmployees[0] : transformedEmployees;
 }
 
 module.exports = getEmployeesCoverage;
